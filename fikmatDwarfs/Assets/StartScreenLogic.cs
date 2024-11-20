2a2b8b38d7c4b1767f49e92f432fc17250a89678
Pepa Bůh <128801641+TheJosephBear@users.noreply.github.com>
2024-11-20 19:34:47 +0100

    player select ready

--- fikmatDwarfs/Assets/StartScreenLogic.cs
+++ fikmatDwarfs/Assets/StartScreenLogic.cs
@@ -41,7 +41,7 @@ public class StartScreenLogic : MonoBehaviour {
         // introCutscene.gameObject.SetActive(false);
         // Show the UI for player choosing and keep animation frozen on last frame
         introPlaying = false;
-        ShowStartUI();
+        FindAnyObjectByType<PlayerSelectLogic>().Initialize();
     }
 
     void SkipIntro() {
@@ -50,87 +50,11 @@ public class StartScreenLogic : MonoBehaviour {
         introCutscene.Evaluate();
         introCutscene.Stop();
         introPlaying = false;
-        ShowStartUI();
+        print(FindAnyObjectByType<PlayerSelectLogic>());
+        FindAnyObjectByType<PlayerSelectLogic>().Initialize();
     }
 
-    void ShowStartUI() {
-        print("ShowStartUI");
-        UtilityUI.Fade(playerJoinUI, true, 0.2f);
-        ShowPlayerMessage(true, "Stiskni èervené tlaèítko");
-        ShowPlayerMessage(false, "Stiskni èervené tlaèítko");
-    }
-
-    void ShowPlayerMessage(bool isPlayerOne, string message) {
-        if (isPlayerOne) {
-            playerOneMessage.text = message;
-        } else {
-            playerTwoMessage.text = message;
-        }
-    }
-
-    void StartCountdown() {
-        // Start countdown if not already started
-        if (countdownCoroutine == null) {
-            countdownCoroutine = StartCoroutine(CountdownCoroutine());
-        }
-    }
-
-    IEnumerator CountdownCoroutine() {
-        float timeRemaining = countdownTime;
-        while (timeRemaining >= 0) {
-            yield return new WaitForSeconds(1f);
-            countdownText.text = ((int)timeRemaining).ToString();
-            timeRemaining--;
-        }
-
-        // Proceed to start cutscene once countdown ends or both players are ready
-        StartCoroutine(IntroCutscene());
-    }
-
-    IEnumerator IntroCutscene() {
-        print("intro cutscene");
-        playerJoinUI.SetActive(false);
-        // Example animation to move the canvas out of the screen
-
-        // Animate UI u
-        float animationTime = 1f;
-        float elapsedTime = 0f;
-        Vector3 startPosition = playerJoinUI.transform.position;
-
-        while (elapsedTime < animationTime) {
-        //    playerJoinUI.transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / animationTime);
-            cameraIntro.transform.position = Vector3.Lerp(startPosition, CameraAnimationTarget, elapsedTime / animationTime);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-        print("Camera shoupld be looking down");
-
-        // Trigger game start
-        FindAnyObjectByType<GamePlayLogic>().StartGame();
-    }
-
-    void OnPlayerJoin(bool isPlayerOne) {
-        if (introPlaying) {
-            SkipIntro();
-            return;
-        }
-
-        ShowPlayerMessage(isPlayerOne, "Pøipraven");
-        if (isPlayerOne) {
-            playerOneJoined = true;
-        } else {
-            playerTwoJoined = true;
-        }
-
-        if (playerOneJoined && playerTwoJoined) {
-            // Both players joined, stop countdown and start cutscene
-            //   StopCoroutine(countdownCoroutine);
-            //   StartCoroutine(IntroCutscene());
-        } else if (playerOneJoined || playerTwoJoined) {
-            // Start countdown when only player one has joined
-            StartCountdown();
-        }
-    }
+    
 
     void OnEnable() {
         InputManager.Instance?.SubscribeToAction("SecondaryOne", OnPrimaryOne);
@@ -144,13 +68,13 @@ public class StartScreenLogic : MonoBehaviour {
 
     void OnPrimaryOne(InputAction.CallbackContext context) {
         if (context.ReadValue<float>() == 1f && !playerOneJoined) {
-            OnPlayerJoin(true);
+            if (introPlaying) SkipIntro();
         }
     }
 
     void OnPrimaryTwo(InputAction.CallbackContext context) {
         if (context.ReadValue<float>() == 1f && !playerTwoJoined) {
-            OnPlayerJoin(false);
+            if (introPlaying) SkipIntro();
         }
     }
 }
